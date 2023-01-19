75852f4c3b90a47dadf7ce92803b256c76518cb8
Brenda Bispo <115106207+brendabispo@users.noreply.github.com>
2023-01-19 20:47:00 -0300

    iniciando a criação do crud

--- src/main/java/com/escolabba/infra/aluno/RepositorioDeAlunosComJDBC.java
+++ src/main/java/com/escolabba/infra/aluno/RepositorioDeAlunosComJDBC.java
@@ -4,13 +4,19 @@ import com.escolabba.dominio.aluno.Aluno;
 import com.escolabba.dominio.aluno.Cpf;
 import com.escolabba.dominio.aluno.RepositorioDeAlunos;
 
+import java.sql.Connection;
 import java.util.List;
 
 public class RepositorioDeAlunosComJDBC implements RepositorioDeAlunos {
 
+    private final Connection connection;
+
+    public RepositorioDeAlunosComJDBC(Connection connection) {
+        this.connection = connection;
+    }
+
     @Override
     public void matricular(Aluno aluno) {
-
     }
 
     @Override
